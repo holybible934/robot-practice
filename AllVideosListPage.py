94588f9555b352dbc340450c50951c006c00d13d
CHChang <holybible934@gmail.com>
2020-10-12 23:27:04 +0800

    Use function to check whether uploader exists in uploaded videos. If yes, then click that video; Otherwise do nothing

--- AllVideosListPage.py
+++ AllVideosListPage.py
@@ -3,12 +3,18 @@ import time
 from PageObjectLibrary import PageObject
 
 
+def uploader_exists_in_uploads(uploader, uploads):
+    for j in range(len(uploads)):
+        print('uploads[j] is ' + uploads[j].text)
+        if uploads[j].text == uploader:
+            return uploads[j]
+
+
 class AllVideosListPage(PageObject):
     PAGE_TITLE = 'Twitch 搜尋 - MONSTER HUNTER WORLD'
     PAGE_URL = '/search?category=vods&term=%20MONSTER%20HUNTER%20WORLD'
 
     _locators = {
-        'videos_list': 'xpath=//*[@id="__next"]/div/div/main/div/div/div[2]/div',
         'videos': 'css=p[class^=tw-c-text-alt-2]'
     }
 
@@ -19,13 +25,17 @@ class AllVideosListPage(PageObject):
             raise Exception(message)
         return True
 
-    def scroll_down_to_the_first_video(self, uploader):
-        videos = self.selib.find_elements(self.locator.videos)
-        for i in range(5):
-            if len(videos) > 0:
-                for j in range(len(videos)):
-                    print(videos[j].text)
-                print('-------------------')
-                self.driver.execute_script('arguments[0].scrollIntoView();', videos[len(videos) - 1])
-                videos = self.selib.find_elements(self.locator.videos)
+    def scroll_down_to_the_first_video(self, uploader, target=None):
+        uploads = self.selib.find_elements(self.locator.videos)
+        print(uploader)
+        for i in range(50):
+            if len(uploads) > 0:
+                target = uploader_exists_in_uploads(uploader, uploads)
+                self.driver.execute_script('arguments[0].scrollIntoView();', uploads[len(uploads) - 1])
+                uploads = self.selib.find_elements(self.locator.videos)
                 time.sleep(1)
+            if target is not None:
+                break
+            else:
+                return
+        self.selib.find_element(target).click()
